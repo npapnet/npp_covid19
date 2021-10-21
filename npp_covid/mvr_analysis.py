b07fce3adb68dabe84fd90e9ab0531eec2990451
npapnet <npapnet@gmail.com>
2021-10-21 18:55:01 +0300

    removed mvr unncessary files.

--- npp_covid/mvr_analysis.py
+++ npp_covid/mvr_analysis.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 
-from npp_covid.io.heroku import hag_create_df
-from npp_covid.misc.smooth_funcs import prepare_cases_gaussian
+# from npp_covid.io.heroku import hag_create_df
+# from npp_covid.misc.smooth_funcs import prepare_cases_gaussian
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import mean_squared_error
 import seaborn as sns
 
 import  statsmodels.api as sm
