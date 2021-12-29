434010c6ff1aa1b3054fff7aa08ab79e308e4541
npapnet <npapnet@gmail.com>
2021-12-29 18:15:03 +0200

    Added TimeSeries tools folder with tests.

---
+++ npp_covid/ts_tools/TS_check.py
@@ -0,0 +1,136 @@
+#%%
+import pandas as pd
+import numpy as np
+
+def get_no_days(ds:pd.Series)-> int:
+    """gets number of days in time series based on last and first day
+
+    Args:
+        ds (pd.Series): [description]
+
+    Returns:
+        int: [description]
+    """    
+    return (ds.index[-1]-ds.index[0]).days+1
+
+def is_contiguous(ds:pd.Series)-> bool:
+    """checks if dataTime index is contiguous
+
+    Args:
+        ds (pd.Series): [description]
+
+    Returns:
+        bool: [description]
+    """    
+    clean = ds.dropna()
+    is_cont = True
+    # if clean.shape != ds.shape:
+    #     is_cont = False
+    if clean.shape != gen_continuous_DTindex(clean).shape:
+        is_cont = False
+    return is_cont
+
+def gen_continuous_DTindex(ds:pd.Series)->pd.DatetimeIndex:
+    """Generates a continuous DateTimeindex which 
+    - starts from the first date of the ds.index
+    - ends   on the final date of the ds.index
+    checks that the index is of type pd.DateTimendex
+
+    Args:
+        ds (pd.Series): pandas.Series with index a pd.DatetimeIndex
+
+    Returns:
+        pd.DatetimeIndex: [description]
+    """
+    assert is_index_date_time(ds)
+    new_index = pd.date_range(start=ds.index[0], end =ds.index[-1])
+    return new_index
+
+
+def is_index_date_time(ds:pd.Series)->bool:
+    """checks whether the index of the time Series is of type DatetimeIndex
+
+    Args:
+        ds (pd.Series): [description]
+
+    Returns:
+        bool: [description]
+    """    
+    return  isinstance(ds.index, pd.DatetimeIndex)
+# %%
+class TS_Index_Checker():
+    def __init__(self, ds:pd.Series) -> None:
+        self._ds = ds
+        self._clean = ds.dropna()
+        
+        # Initial checks
+        if not is_index_date_time(ds):
+            raise(TypeError("Index type is not pd.DatetimeIndex"))
+    
+    def _analyze(self)-> dict:
+        metadata = {}
+        metadata['length'] = len(self._ds)
+        metadata['na'] = self._ds.isna().sum()
+        metadata['largest_segment'] = find_largest_contiguous_segment(self._clean)
+        return metadata
+    
+    def get_largest_continuous_segment(self):
+        inds = find_largest_contiguous_segment(self._clean)
+        new_ds = self._clean.iloc[inds[0]:inds[1]]
+        return new_ds
+        
+    def is_suitable(self):
+        return self.is_contiguous(self.ds)
+    
+def find_contiguous_segments(ds:pd.Series)->list:
+    """REturns a list of list with the indexes of contiguous segments
+    All segments are returned
+
+    Args:
+        ds (pd.Series): ds with na removed
+
+    Returns:
+        list: list of [start_index, end_index] 
+    """    
+    day_diff = (ds.index[1:]-ds.index[:-1]).days.values
+    ioi = np.where(day_diff>1)[0] #indexes of interest
+    
+    segment_indxs = []
+    tmp = [0]
+    if ioi.size>0:
+        for indx in ioi:
+            tmp.append(indx)
+            segment_indxs.append(tmp)
+            tmp = [indx+1]
+    tmp.append(len(ds)-1)
+    segment_indxs.append(tmp)
+    
+    return segment_indxs
+    
+def find_largest_contiguous_segment(ds:pd.Series)->list:
+    all_segs = find_contiguous_segments(ds)
+    index_len = 0
+    max_len = 0 
+    for k in range(len(all_segs)):
+        seg = all_segs[k]
+        seg_len = seg[1]-seg[0]+1
+        if (seg_len>max_len):
+            max_len = seg_len
+            index_len = k
+
+    return all_segs[index_len]    
+
+# %%
+if __name__ =="__main__":
+    def create_ds(no_periods=5):
+        indx = pd.date_range(start='2021-12-01', periods=no_periods)
+        ds = pd.Series(range(no_periods), index = indx)
+        return ds
+    no_periods=15
+    ds = create_ds(no_periods)
+    ds2 = ds.replace(4,  np.nan)    
+    oa = TS_Index_Checker(ds=ds2)
+    
+    
+    
+# %%
---
+++ tests/ts_tools/test_TS_check.py
@@ -0,0 +1,102 @@
+#%%
+import pytest
+import pandas as pd
+import numpy as np
+
+import npp_covid.ts_tools.TS_check as tsc
+
+def create_ds(no_periods=5):
+    indx = pd.date_range(start='2021-12-01', periods=no_periods)
+    ds = pd.Series(range(no_periods), index = indx)
+    return ds
+
+
+def test_get_days():
+    indx = pd.date_range(start='2021-12-01', end ='2021-12-05')
+    ds = pd.Series(0, index = indx)
+    assert tsc.get_no_days(ds) == 5
+
+def test_get_days2():
+    no_periods=5
+    ds = create_ds(no_periods)
+    assert tsc.get_no_days(ds) == no_periods
+
+
+def test_is_contiguous_length():
+    """checks if a ds is an NA in the middle is contiguous (based on length)
+    """    
+    no_periods=5
+    ds = create_ds(no_periods)
+    ds2 = ds.replace(2,  np.nan)
+    assert (tsc.is_contiguous(ds2) == False)
+
+def test_is_contiguous_edge():
+    """checks if a ds is an NA on the edge  is contiguous (based on length)
+    """    
+    no_periods=5
+    ds = create_ds(no_periods)
+    ds2 = ds.replace(0,  np.nan)
+    assert tsc.is_contiguous(ds2) == True
+    ds2 = ds.replace(no_periods-1,  np.nan)
+    assert tsc.is_contiguous(ds) == True
+
+def test_is_index_date_time():
+    no_periods=5
+    ds = create_ds(no_periods)
+    assert tsc.is_index_date_time(ds)
+    
+def test_find_contiguous_segments():
+    no_periods=15
+    ds = create_ds(no_periods)
+    ds2 = ds.replace(4,  np.nan).dropna()
+
+    assert (tsc.find_contiguous_segments(ds2) == [[0, 3], [4, 13]])
+    assert (tsc.find_contiguous_segments(ds) == [[0, 14]])
+
+
+def test_find_largest_contiguous_segment():
+    no_periods=15
+    ds = create_ds(no_periods)
+    assert (tsc.find_largest_contiguous_segment(ds)== [0, 14])
+    
+    ds2 = ds.replace(4,  np.nan).dropna()
+    assert (tsc.find_largest_contiguous_segment(ds2) == [4, 13])
+
+def test_analyze():
+    no_periods=15
+    ds = create_ds(no_periods)
+    
+    oa = tsc.TS_Index_Checker(ds=ds)
+    assert (oa._analyze() == {'length':15, 'na':0,
+                'largest_segment': [0,14]})
+
+    ds2 = ds.replace(4,  np.nan)    
+    oa = tsc.TS_Index_Checker(ds=ds2)
+    assert (oa._analyze() == {'length':15, 'na':1,
+                'largest_segment': [4,13]})
+    
+    ds2a= ds2.dropna()
+    oa = tsc.TS_Index_Checker(ds=ds2a)
+    assert (oa._analyze() == {'length':14, 'na':0,
+                'largest_segment': [4,13]})
+  
+
+def test_analyze_end():
+    """tests analyze function of TS_Index_Checker
+    """    
+    no_periods=15
+    ds = create_ds(no_periods)
+    
+    oa = tsc.TS_Index_Checker(ds=ds)
+    assert (oa._analyze() == {'length':15, 'na':0,
+                'largest_segment': [0,14]})
+
+    ds2 = ds.replace(14,  np.nan)    
+    oa = tsc.TS_Index_Checker(ds=ds2)
+    assert (oa._analyze() == {'length':15, 'na':1,
+                'largest_segment': [0,13]})
+
+    ds2a= ds2.dropna()
+    oa = tsc.TS_Index_Checker(ds=ds2a)
+    assert (oa._analyze() == {'length':14, 'na':0,
+                'largest_segment': [0,13]})
